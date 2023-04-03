ee3ed2bd8b4c027f968b260eeda817dc97db8979
Ali Çukur <alicukur.yzlm@gmail.com>
2023-04-03 18:26:26 +0300

    .

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import {Image, StyleSheet, SafeAreaView, FlatList , ScrollView, Dimensions} from "react-native";
+import {
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import news_data from "./src/news_data.json";
 import NewsCard from "./src/Components/NewsCard";
 import bannerData from "./src/news_banner_data.json";
@@ -9,16 +16,16 @@ export default function App() {
   const keys = (item, index) => item.u_id.toString();
 
   return (
-     <SafeAreaView style={styles.container}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-           {
-            bannerData.map(bannerNews => <Image style={styles.imageStyle} source={{url:bannerNews.imageUrl}}/>)
-           }
-        </ScrollView>
-      <FlatList 
-       keyExtractor={keys}
-       data={news_data} 
-       renderItem={renderNews} />
+    <SafeAreaView style={styles.container}>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {bannerData.map((bannerNews) => (
+          <Image
+            style={styles.imageStyle}
+            source={{ url: bannerNews.imageUrl }}
+          />
+        ))}
+      </ScrollView>
+      <FlatList keyExtractor={keys} data={news_data} renderItem={renderNews} />
     </SafeAreaView>
   );
 }
@@ -29,9 +36,8 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(1,4,10,0.2)",
   },
 
-  imageStyle:{
-    height:Dimensions.get('window').height / 3,
-    width:Dimensions.get('window').width / 2,
-     
-  }
+  imageStyle: {
+    height: Dimensions.get("window").height / 3,
+    width: Dimensions.get("window").width / 2,
+  },
 });
